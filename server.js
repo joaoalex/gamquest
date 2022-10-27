3164db2c7c9d6e5c4f271f4956340f1d759b36bb
joaoalex <joao.m.alexandre@gmail.com>
2022-10-27 20:23:24 +0100

    r

--- server.js
+++ server.js
@@ -1,34 +1,59 @@
 const express = require("express");
-bodyParser = require('body-parser');
-
-const { sequelize } = require("sequelize");
-const cors = require("cors");
-
 const app = express();
-//app.use(bodyParser.json());
-app.use(express.json());
+const port = process.env.PORT || 3001;
 
-var corsOptions = {
-  origin: "*",
-  //["http://localhost:8081", "http://localhost:4200", "http://localhost"],
-  credentials: "true"
-};
+app.get("/", (req, res) => res.type('html').send(html));
 
-app.use(cors(corsOptions));
-const db = require("./app/models");
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
 
-// simple route
-//app.get("/", (req, res) => {
-//  res.json({ message: "Welcome to Agile application." });
-//});
 
-const path = require('path');
-app.use(express.static(path.join(__dirname, 'dist/angular-starter')));
-
-require("./app/routes/quest.routes")(app);
-
-// set port, listen for requests
-const PORT = process.env.PORT || 8080;
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}.`);
-});
+const html = `
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Hello from Render!</title>
+    <script src="https://cdn.jsdelivr.net/npm/canvas-confetti@1.5.1/dist/confetti.browser.min.js"></script>
+    <script>
+      setTimeout(() => {
+        confetti({
+          particleCount: 100,
+          spread: 70,
+          origin: { y: 0.6 },
+          disableForReducedMotion: true
+        });
+      }, 500);
+    </script>
+    <style>
+      @import url("https://p.typekit.net/p.css?s=1&k=vnd5zic&ht=tk&f=39475.39476.39477.39478.39479.39480.39481.39482&a=18673890&app=typekit&e=css");
+      @font-face {
+        font-family: "neo-sans";
+        src: url("https://use.typekit.net/af/00ac0a/00000000000000003b9b2033/27/l?primer=7cdcb44be4a7db8877ffa5c0007b8dd865b3bbc383831fe2ea177f62257a9191&fvd=n7&v=3") format("woff2"), url("https://use.typekit.net/af/00ac0a/00000000000000003b9b2033/27/d?primer=7cdcb44be4a7db8877ffa5c0007b8dd865b3bbc383831fe2ea177f62257a9191&fvd=n7&v=3") format("woff"), url("https://use.typekit.net/af/00ac0a/00000000000000003b9b2033/27/a?primer=7cdcb44be4a7db8877ffa5c0007b8dd865b3bbc383831fe2ea177f62257a9191&fvd=n7&v=3") format("opentype");
+        font-style: normal;
+        font-weight: 700;
+      }
+      html {
+        font-family: neo-sans;
+        font-weight: 700;
+        font-size: calc(62rem / 16);
+      }
+      body {
+        background: white;
+      }
+      section {
+        border-radius: 1em;
+        padding: 1em;
+        position: absolute;
+        top: 50%;
+        left: 50%;
+        margin-right: -50%;
+        transform: translate(-50%, -50%);
+      }
+    </style>
+  </head>
+  <body>
+    <section>
+      Hello from Render!
+    </section>
+  </body>
+</html>
+`
